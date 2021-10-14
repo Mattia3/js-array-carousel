20e1ae5f4e7a7f2f4dcce0a8ceded0f45305de3d
Mattia3 <mcortinovis9@gmail.com>
2021-10-14 22:51:37 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const items = [
-  'img/01.jpg',
-  'img/02.jpg',
-  'img/03.jpg',
-  'img/04.jpg',
-  'img/05.jpg'
+  'consegna/img/01.jpg',
+  'consegna/img/02.jpg',
+  'consegna/img/03.jpg',
+  'consegna/img/04.jpg',
+  'consegna/img/05.jpg'
 ];
 
 const title = [
@@ -26,12 +26,79 @@ const text = [
 
 const imageContainerLeft = document.querySelector(".image-container-left");
 const carosel = document.querySelector(".image-container-right");
+const textImg = document.querySelector(".text-img");
+let imgSelect = 0;
 
-for (let i = 0; i < items.lenght; i++) {
+for (let i = 0; i < items.length; i++) {
   const urlImgCorrent = items[i];
 
   let op0 = "";
+  if (op0 === items[i]){
+    op0 = "opacity-0";
+  }
   
-  const tagImg = '<img class="${op0}" src="${urlImgCorrent}" alt="immagini #${i}">';
+  const tagImg =`<img class="${op0}" src="${urlImgCorrent}" alt="immagini #${i}">`;
   imageContainerLeft.innerHTML += tagImg;
+  console.log("sono arrivato qua");
+
+  carosel.innerHTML += tagImg;
+  
+  let titleText =`<div class="titoli${op0}"><h2>${title[i]}</h2>${text[i]}</div>`
+  imageContainerLeft.innerHTML += titleText;
 }
+
+const selectedText = document.querySelectorAll(".titoli")
+const selectedThumb = document.querySelectorAll(".image-container-right img") 
+const selectedBigImg = document.querySelectorAll(".image-container-left img")
+
+let singolSelectedThumb = selectedThumb[imgSelect];
+let singolSelectedBigImg = selectedBigImg[imgSelect];
+let singolSelectedText = selectedText[imgSelect];
+
+singolSelectedText.classList.add("active")
+singolSelectedThumb.classList.add("active")
+singolSelectedBigImg.classList.add("img-big-active")
+
+const arrowTop = document.querySelector(".arrow-top");
+const arrowBottom = document.querySelector(".arrow-bottom");
+
+
+arrowTop.addEventListener("click", function() {
+  //Riazzero il ciclo e lo faccio tornare dall'inizio
+  if(imgSelect === 0){
+    imgSelect = items.length;
+  }
+  singolSelectedThumb.classList.remove("active");
+  singolSelectedBigImg.classList.remove("img-big-active");
+  singolSelectedText.classList.remove("active");
+  imgSelect--;
+  singolSelectedThumb = selectedThumb[imgSelect];
+  singolSelectedBigImg = selectedBigImg[imgSelect];
+  singolSelectedText = selectedText[imgSelect];
+  singolSelectedThumb.classList.add("active");
+  singolSelectedBigImg.classList.add("img-big-active");
+  singolSelectedText.classList.add("active");
+})
+
+arrowBottom.addEventListener("click", function() {
+  //Riazzero il ciclo e lo faccio tornare dall'inizio
+  if(imgSelect === items.length -1){
+    imgSelect = -1;
+  }
+  singolSelectedThumb.classList.remove("active");
+  singolSelectedBigImg.classList.remove("img-big-active")
+  singolSelectedText.classList.remove("active");
+  imgSelect++;
+  singolSelectedThumb = selectedThumb[imgSelect];
+  singolSelectedBigImg = selectedBigImg[imgSelect];
+  singolSelectedText = selectedText[imgSelect];
+  singolSelectedThumb.classList.add("active")
+  singolSelectedBigImg.classList.add("img-big-active")
+  singolSelectedText.classList.add("active");
+  
+})
+
+
+
+//titoli nascosti 
+//
